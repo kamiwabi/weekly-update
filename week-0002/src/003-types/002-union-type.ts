9343efd719bcb3102948b46e9849aa051ec52891
Bambang Purnomosidi D. P <phylossophie@gmail.com>
2020-02-28 14:54:34 +0700

    plus ASC

---
+++ week-0002/src/003-types/002-union-type.ts
@@ -0,0 +1,12 @@
+function padLeft(value: string, padding: string | number) {
+    if (typeof padding === "number") {
+        return Array(padding + 1).join(" ") + value;
+    }
+    if (typeof padding === "string") {
+        return padding + value;
+    }
+    throw new Error(`Expected string or number, got '${padding}'.`);
+}
+
+console.log(padLeft("Hello world", 4)); // returns "    Hello world"
+console.log(padLeft("Hello world", "from Wabi - ")); 
